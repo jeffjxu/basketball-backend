0abb2987346ee59b772a97787a6f0c8e675cdafa
Jeff Xu <jiaxijeffxu@gmail.com>
2020-10-22 15:21:59 -0400

    create favorite model and test

--- BasketballAPI/app/models/favorite.rb
+++ BasketballAPI/app/models/favorite.rb
@@ -1,4 +1,15 @@
 class Favorite < ApplicationRecord
-  belongs_to :favoriter, :class_name => "User"
-  belongs_to :favoritee, :class_name => "User"
+  # Relationships
+  belongs_to :favoriter, :class_name => "User", :foreign_key => "favoriter_id"
+  belongs_to :favoritee, :class_name => "User", :foreign_key => "favoritee_id"
+
+  # Validations
+  validates_presence_of :favoriter_id, :favoritee_id
+  validates_uniqueness_of :favoriter_id, scope: %i[favoritee_id]
+
+  # Scopes
+  scope :for_favoriter, -> (favoriter_id) { where('favoriter_id = ?', favoriter_id) }
+  scope :for_favoritee, -> (favoritee_id) { where('favoritee_id = ?', favoritee_id) }
+
+
 end
