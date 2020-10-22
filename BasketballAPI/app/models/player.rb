9c5bfbdc601288c57dc6ffb9bcaa13b5de833e95
Jeff Xu <jiaxijeffxu@gmail.com>
2020-10-21 21:46:42 -0400

    create player model and test

--- BasketballAPI/app/models/player.rb
+++ BasketballAPI/app/models/player.rb
@@ -1,4 +1,16 @@
 class Player < ApplicationRecord
+  # Relationships
   belongs_to :user
   belongs_to :game
+
+  # Validations
+  validates_presence_of :user_id, :game_id, :status
+  validates_inclusion_of :status, in: %w[invited going maybe], message: "is not an option", allow_blank: true
+
+  # Scopes
+  scope :invited, -> { where(status: 'invited') }
+  scope :going, -> { where(status: 'going') }
+  scope :maybe, -> { where(status: 'maybe') }
+  scope :for_game, -> (game_id) { where('game_id = ?', game_id) }
+  scope :for_user, -> (user_id) { where('user_id = ?', user_id) }
 end
