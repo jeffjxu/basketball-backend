912321e1ed218c13e32f083f1b72e49e7506e876
Jeff Xu <jiaxijeffxu@gmail.com>
2020-11-29 13:02:51 -0500

    add for_user and for_game path for player

--- BasketballAPI/config/routes.rb
+++ BasketballAPI/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'players/for_user', to: 'players#for_user', as: :players_for_user
+  get 'players/for_game', to: 'players#for_game', as: :players_for_game
+
   resources :users
   resources :games
   resources :players
--- BasketballAPI/test/controllers/players_controller_test.rb
+++ BasketballAPI/test/controllers/players_controller_test.rb
@@ -1,31 +1,41 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
-  # setup do
-  #   @user = FactoryBot.create(:user, username: "michaeljordan1", email: "michaeljordan@gmail.com", firstname: "Michael", lastname: "Jordan", dob: 10.years.ago.to_date)
-  #   @game = FactoryBot.create(:game, name: "game12", date: Date.current, time: Time.current, description: "a basketball game 1", private: false, longitude: 0.0, latitude: 0.0)
-  #   @player = FactoryBot.create(:player, status: "going", user: @user, game: @game)
-  # end
+  setup do
+    @user = FactoryBot.create(:user, username: "michaeljordan1", email: "michaeljordan@gmail.com", firstname: "Michael", lastname: "Jordan", dob: 10.years.ago.to_date)
+    @game = FactoryBot.create(:game, name: "game12", date: Date.current, time: Time.current, description: "a basketball game 1", private: false, longitude: 0.0, latitude: 0.0)
+    @player = FactoryBot.create(:player, status: "going", user: @user, game: @game)
+  end
 
-  # test "should get index" do
-  #   get players_path
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get players_path
+    assert_response :success
+  end
 
-  # test "should update player" do
-  #   patch player_path(@player), params: { status: "maybe", user_id: @user.id, game_id: @game.id }
-  #   assert_response :success
-  # end
+  test "should update player" do
+    patch player_path(@player), params: { status: "maybe", user_id: @user.id, game_id: @game.id }
+    assert_response :success
+  end
 
-  # test "should show player" do
-  #   get player_path(@player)
-  #   assert_response :success
-  # end
+  test "should show player" do
+    get player_path(@player)
+    assert_response :success
+  end
 
-  # test "should destroy player" do
-  #   assert_difference('Player.count', -1) do
-  #     delete player_path(@player)
-  #     assert_response :success
-  #   end
-  # end
+  test "should destroy player" do
+    assert_difference('Player.count', -1) do
+      delete player_path(@player)
+      assert_response :success
+    end
+  end
+
+  test "should have for_user path" do
+    get players_for_user_path
+    assert_response :success
+  end
+
+  test "should have for_game path" do
+    get players_for_game_path
+    assert_response :success
+  end
 end
