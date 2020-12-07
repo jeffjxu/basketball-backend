c74b8527c1beee1d63ac409998ef2b971414d527
Jeff Xu <jiaxijeffxu@gmail.com>
2020-12-07 14:48:09 -0800

    add not going for players

--- BasketballAPI/app/models/player.rb
+++ BasketballAPI/app/models/player.rb
@@ -5,13 +5,14 @@ class Player < ApplicationRecord
 
   # Validations
   validates_presence_of :user_id, :game_id, :status
-  validates_inclusion_of :status, in: %w[invited going maybe], message: "is not an option", allow_blank: true
+  validates_inclusion_of :status, in: %w[invited going maybe not_going], message: "is not an option", allow_blank: true
   validates_uniqueness_of :user_id, scope: %i[game_id]
 
   # Scopes
   scope :invited, -> { where(status: 'invited') }
   scope :going, -> { where(status: 'going') }
   scope :maybe, -> { where(status: 'maybe') }
+  scope :not_going, -> { where(status: 'not_going') }
   scope :for_game, -> (game_id) { where('game_id = ?', game_id) }
   scope :for_user, -> (user_id) { where('user_id = ?', user_id) }
   scope :alphabetical, ->  { joins(:user).order('firstname, lastname') }
